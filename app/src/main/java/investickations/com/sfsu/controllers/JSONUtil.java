f5144bea2eb02e99342ba71fec2836f6fda74c6e
pmk2429 <pmk.2429@gmail.com>
2015-05-27 18:42:15 -0700

    changes in utility classes

--- app/src/main/java/investickations/com/sfsu/controllers/JSONUtil.java
+++ app/src/main/java/investickations/com/sfsu/controllers/JSONUtil.java
@@ -9,6 +9,13 @@ import investickations.com.sfsu.entities.*;
 /**
  * Created by Pavitra on 5/19/2015.
  */
+
+/**
+ * JSONUtil class is the Utility class which provides helps to create the JSON Parser
+ * for each of the entity in the application. This JSON Parser are used to retrieve the
+ * data from remote connection.
+ * The data representation of data retrieved are in JSOn format.
+ */
 public class JSONUtil {
     static public class UserJSONParser {
         static ArrayList<User> parseUser(String in) throws JSONException {
---
+++ gitScript.sh
@@ -0,0 +1,3 @@
+git add -a
+git commit -m $1 $2 $3
+git push origin master
