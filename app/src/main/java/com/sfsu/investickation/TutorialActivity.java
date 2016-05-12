b7e0187966f1e0c87c5cf9b028b3a7d0e39a4de1
Pavitra Kansara <pmk.2429@gmail.com>
2016-05-12 13:02:38 -0700

    Add initial version of Tutorial with Screenshots for better usablity and navigatiing through Application

--- app/src/main/java/com/sfsu/investickation/TutorialActivity.java
+++ app/src/main/java/com/sfsu/investickation/TutorialActivity.java
@@ -2,8 +2,8 @@ package com.sfsu.investickation;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.sfsu.investickation.fragments.TutorialFragment;
 
 
 /**
@@ -28,6 +28,12 @@ public class TutorialActivity extends MainBaseActivity {
             if (savedInstanceState != null) {
                 return;
             }
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.tutorial_fragment_container, new TutorialFragment())
+                    .commit();
+
         }
     }
 
@@ -44,25 +50,4 @@ public class TutorialActivity extends MainBaseActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_personal_info, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the HomeFragment/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/sfsu/investickation/fragments/TutorialFragment.java
@@ -0,0 +1,30 @@
+package com.sfsu.investickation.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sfsu.investickation.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TutorialFragment extends Fragment {
+
+
+    public TutorialFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tutorial, container, false);
+    }
+
+}
